9daedd1d839c54df938e82a178dca06eef9bf255
github username <github email address>
2019-07-10 17:15:21 +0100

    updated Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,18 +7,23 @@ class UsersController < ApplicationController
   def show
     user = User.find_by(id: parms[:id])
     if user
-      render json: user
+      render json: user, include: [:user_comments, { 
+        :user_comments => { 
+          include: :comment, except: [:updated_at, :created_at]
+           }
+      }],
+      except: [:updated_at, :created_at]
     else
       render json: { error: 'User not found from SHOW server.' }, status: 404
     end
   end
 
   def create
-    user = User.new(user: parms[:user])
+    user = User.new(username: params[:username], password: params[:password])
     if user.save
       render json: user
     else
-      render json: { error: "User can not generate from CREATE" }, status: 400
+      render json: {error: "Username Already Taken! from SIGNUP server"}, status: 400
     end
   end
 
@@ -31,6 +36,24 @@ class UsersController < ApplicationController
   def destroy
     user = User.find(params[:id])
     user.destroy
+  end
+
+  def signin
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      render json: { username: user.username, token: issue_token({ id: user.id }) }
+    else
+      render json: { error: 'Invalid username/password combination from SIGNIN server.' }, status: 401
+    end
+  end
+
+  def validate
+    user = current_user
+    if user
+      render json: { username: user.username, token: issue_token({ id: user.id }) }
+    else
+      render  status: 404
+    end
   end
 
 end
